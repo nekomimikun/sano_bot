d45cc8c97e6d2bf8003479d3268ededfd1b0c613
wassan128 <wassan.taiko.master@gmail.com>
2017-07-21 14:23:23 +0900

    fix bugs

--- bot_rest.py
+++ bot_rest.py
@@ -3,21 +3,21 @@ import twitter
 import sqlite3
 import os,sys
 sys.path.append(os.pardir)
-from name import ACTION_TOKEN
-from name import ACTION_SECRET
+from name import ACCESS_TOKEN
+from name import ACCESS_SECRET
 from name import CONSUMER_KEY
 from name import CONSUMER_SECRET
 
-auth = twitter.OAuth(ACTION_TOKEN,ACTION_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
+auth = twitter.OAuth(ACCESS_TOKEN,ACCESS_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
 
 t_rest = twitter.Twitter(auth = auth)
 
-dbname = '../../bot_lib.db'
+dbname = "../../bot_lib.db"
 conn = sqlite3.connect(dbname)
 c = conn.cursor()
 
-c.execute('select * from Library')
-list = c.fetchall()
+c.execute("select * from Library")
+lst = c.fetchall()
 
-tweet = str(list[random.random(0,len(list))]) + "さの先輩"
+tweet = str(lst[random.random(0,len(lst))]) + "さの先輩"
 t_rest.statuses.update(status = tweet)
